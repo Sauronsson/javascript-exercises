b41fcf78c4bc98f56df48285589de982cabedb2d
Austin Hunt <ahunt03087@gmail.com>
2024-05-10 21:17:48 -0400

    completed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(aString, amountRepetition) {
+    if(amountRepetition < 0){
+        return "ERROR"
+    }
+    
+    let combinatorString = ""
+    for(let i = 0; i < amountRepetition; i++)
+    {
+        combinatorString += aString;
+    }
+    return combinatorString;
 };
 
 // Do not edit below this line
