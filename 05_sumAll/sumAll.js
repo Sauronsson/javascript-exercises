38f76320bf5c53166dea3c334198fcbbe20984c5
Austin Hunt <ahunt03087@gmail.com>
2024-05-10 21:46:03 -0400

    finished exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if(num1 <= 0 || num2 <= 0)
+    {
+        return "ERROR";
+    }
+    if(typeof(num1)!="number" || typeof(num2)!="number")
+    {
+        return "ERROR";
+    }
 
+    let bigNumber;
+    let smallNumber;
+    if(num1 > num2)
+    {
+        bigNumber = num1;
+        smallNumber = num2;
+    } 
+    else
+    {
+        smallNumber = num1;
+        bigNumber = num2;
+    }
+
+    let sum = 0;
+    for(let i = smallNumber; i <= bigNumber; i++)
+    {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
