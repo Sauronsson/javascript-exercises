b038a68389546a12b0617190c51395a7864ba79a
Austin Hunt <ahunt03087@gmail.com>
2024-05-10 21:39:01 -0400

    finished exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(aArray, ...itemToBeRemoved) {
+    const arrayWithoutRemovedItems = []
+    for (const item of aArray)
+    {
+        let keepItem = true;
+        for(const removableItem of itemToBeRemoved)
+        {
+            if(item === removableItem)
+            {
+                keepItem = false;
+            }
+        }
 
+        if(keepItem)
+        {
+            arrayWithoutRemovedItems.push(item);
+        }
+    }
+    return arrayWithoutRemovedItems
 };
 
 // Do not edit below this line
