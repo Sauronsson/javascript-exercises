4eefb612eab068155d5024aa7dda29d570b6700c
Austin Hunt <ahunt03087@gmail.com>
2024-05-10 21:59:20 -0400

    Exercise 7 completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+// Brought this in, as it let a user define their own precision. Seemed better than base javascript options
+function round(num, precision) {
+  const factor = Math.pow(10, precision)
+  return Math.round(num * factor) / factor
+}
+
+const convertToCelsius = function(temperature) {
+  return round((temperature-32)*(5/9), precision=1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  return round((temperature*(9/5)+32), precision=1);
 };
 
 // Do not edit below this line
