c4c40c4e86b4dd5d188e8f76618b27639c61679c
Austin Hunt <ahunt03087@gmail.com>
2025-04-13 17:34:36 -0400

    finished palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    for(let front = 0, back = word.length-1; front < word.length/2; front++,back--){
+        let checkSkips = true
+        while (checkSkips) {
+            if( word[front].toUpperCase() == word[front].toLowerCase() && !(word[front] >= '0' && word[front] <= '9')){
+                front++
+            } else if( word[back].toUpperCase() == word[back].toLowerCase() && !(word[back] >= '0' && word[back] <= '9')){
+                back--
+            } else {
+                checkSkips = false
+            }
+        }
+    
+        if(word[front].toLowerCase() !== word[back].toLowerCase()){
+            return false
+        }
+    }
+    return true
 };
 
 // Do not edit below this line
