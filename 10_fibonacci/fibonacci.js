f9a3a33e0af96c900c023ea8c8221642ca751b42
Austin Hunt <ahunt03087@gmail.com>
2025-04-13 17:51:21 -0400

    finished fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    const inputNumber = Number(number)
+    if(number < 0) { return "OOPS"}
 
+    let fibonacciSequence = [0,1]
+    for(let i = 0; i < number-1; i++){
+        fibonacciSequence.push(fibonacciSequence[i] + fibonacciSequence[i+1])
+    }
+
+    return fibonacciSequence[number]
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
