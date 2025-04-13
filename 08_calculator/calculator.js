adc08d06b415d19851bdef911a24800f66f4ac4f
Austin Hunt <ahunt03087@gmail.com>
2025-04-13 00:36:28 -0400

    finished exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(value1, value2) {
+	return value1 + value2
 };
 
-const subtract = function() {
-	
+const subtract = function(value1, value2) {
+	return value1 - value2
 };
 
-const sum = function() {
-	
+const sum = function(numList) {
+  let sum = 0
+	numList.forEach(number => {
+    sum = sum + number
+  })
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(numList) {
+  if(numList.length === 0) 
+    return 0
+  let sum = 1;
+  numList.forEach(number => {
+    sum = sum * number
+  })
+  return sum
 };
 
-const power = function() {
-	
+const power = function(value1, value2) {
+  let sum = 1
+	for(let i = 0; i < value2; i++ ){
+    sum = value1 * sum
+  }
+  return sum
 };
 
-const factorial = function() {
-	
+const factorial = function(value) {
+	let sum = 1
+  for(let i = 1; i <= value; i++){
+    sum = sum * i 
+  }
+  return sum
 };
 
 // Do not edit below this line
