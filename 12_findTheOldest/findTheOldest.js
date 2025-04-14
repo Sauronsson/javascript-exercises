de0d7fffe147371be91231706f212a33b5a14d79
Austin Hunt <ahunt03087@gmail.com>
2025-04-13 21:18:17 -0400

    finished find the oldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    calculateAge = (person) => {
+        if(person.yearOfDeath){
+            return personAge = person.yearOfDeath - person.yearOfBirth
+        } else {
+            currentYear = new Date().getFullYear()
+            return currentYear - person.yearOfBirth
+        }
+    }
+    
+    return people.reduce((oldestPerson, person) => {
+        oldestPersonAge = calculateAge(oldestPerson)
+        personAge = calculateAge(person)
+        if(personAge > oldestPersonAge) {
+            oldestPerson = person
+        }
+        return oldestPerson
+    })
 };
 
 // Do not edit below this line
